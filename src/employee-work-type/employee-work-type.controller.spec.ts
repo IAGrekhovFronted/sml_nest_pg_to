87047599891cdcb09a63b8fa04ceca1f4a4c9ab5
IAGrekhovFronted <iagrekhov.fronted@gmail.com>
2024-11-24 08:11:28 +0500

    Тесты

--- src/employee-work-type/employee-work-type.controller.spec.ts
+++ src/employee-work-type/employee-work-type.controller.spec.ts
@@ -2,13 +2,29 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { EmployeeWorkTypeController } from './employee-work-type.controller';
 import { EmployeeWorkTypeService } from './employee-work-type.service';
 
+import { EmployeeWorkType } from './employeeWorkType.entity';
+import { EmployeeType } from '../employee-type/employeeType.entity';
+import { WorkRequest } from '../work-request/workRequest.entity';
+
 describe('EmployeeWorkTypeController', () => {
   let controller: EmployeeWorkTypeController;
+
+  const mockEmployeeWorkTypeService = {
+    findAll: jest.fn(),
+    createWorkType: jest.fn(),
+    deleteWorkType: jest.fn(),
+    patchWorkType: jest.fn()
+  }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [EmployeeWorkTypeController],
-      providers: [EmployeeWorkTypeService],
+      providers: [
+        {
+          provide: EmployeeWorkTypeService,
+          useValue: mockEmployeeWorkTypeService
+        }
+      ],
     }).compile();
 
     controller = module.get<EmployeeWorkTypeController>(EmployeeWorkTypeController);
@@ -17,4 +33,61 @@ describe('EmployeeWorkTypeController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('should be find', async () => {
+    const employeeWorkType: EmployeeWorkType[] = [
+      {
+        id: 1,
+        title: "Mock Work Type",
+        price: 500,
+        workRequest: [new WorkRequest()],
+        employeeType: [new EmployeeType]
+      }
+    ]
+    mockEmployeeWorkTypeService.findAll.mockReturnValue(employeeWorkType)
+
+    const result = await controller.findAllWorkType()
+
+    expect(result).toEqual(employeeWorkType)
+  })
+
+  it('should be create', async () => {
+    const data: Partial<EmployeeWorkType> = {
+      title: "Mock Work Type",
+      price: 500,
+      workRequest: [new WorkRequest()],
+      employeeType: [new EmployeeType]
+    }
+
+    mockEmployeeWorkTypeService.createWorkType.mockResolvedValue('Success create')
+
+    const result = await controller.createWorkType(data)
+    expect(result).toEqual('Success create')
+  })
+
+  it('should be delete', async () => {
+    const id = 1
+    mockEmployeeWorkTypeService.deleteWorkType.mockReturnValue("Success delete")
+
+    const result = await controller.deleteWorkType({ id: id })
+    expect(result).toEqual("Success delete")
+  })
+
+  it('should be update', async () => {
+    const updateData = {
+      id: 1,
+      data: {
+        title: "Mock Work Type",
+        price: 500,
+        workRequest: [new WorkRequest()],
+        employeeType: [new EmployeeType]
+      }
+    }
+    mockEmployeeWorkTypeService.patchWorkType.mockReturnValueOnce('Success update')
+
+    const result = await controller.updateWorkType(updateData)
+
+    expect(result).toEqual('Success update')
+  })
 });
+
--- src/employee-work-type/employee-work-type.service.spec.ts
+++ src/employee-work-type/employee-work-type.service.spec.ts
@@ -1,12 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EmployeeWorkTypeService } from './employee-work-type.service';
+import { EmployeeWorkType } from './employeeWorkType.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
+import { WorkRequest } from '../work-request/workRequest.entity';
+import { EmployeeType } from '../employee-type/employeeType.entity';
 
 describe('EmployeeWorkTypeService', () => {
   let service: EmployeeWorkTypeService;
 
+  const mockEmployeeWorkType = {
+    find: jest.fn(),
+    delete: jest.fn(),
+    update: jest.fn(),
+    create: jest.fn(),
+    save: jest.fn(),
+  }
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [EmployeeWorkTypeService],
+      providers: [
+        EmployeeWorkTypeService,
+        {
+          provide: getRepositoryToken(EmployeeWorkType),
+          useValue: mockEmployeeWorkType
+        }
+      ],
     }).compile();
 
     service = module.get<EmployeeWorkTypeService>(EmployeeWorkTypeService);
@@ -15,4 +34,60 @@ describe('EmployeeWorkTypeService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should be find', async () => {
+    const employeeWorkType: EmployeeWorkType[] = [
+      {
+        id: 1,
+        title: "Mock Work Type",
+        price: 500,
+        workRequest: [new WorkRequest()],
+        employeeType: [new EmployeeType]
+      }
+    ]
+    mockEmployeeWorkType.find.mockReturnValue(employeeWorkType)
+    const result = await service.findAll()
+
+    expect(result).toEqual(employeeWorkType)
+  })
+
+  it('should be create', async () => {
+    const data: Partial<EmployeeWorkType> = {
+      title: "Mock Work Type",
+      price: 500,
+      workRequest: [new WorkRequest()],
+      employeeType: [new EmployeeType]
+    }
+
+    mockEmployeeWorkType.create.mockReturnValue(data)
+    mockEmployeeWorkType.save.mockResolvedValue("Success save")
+
+    const result = await service.createWorkType(data)
+    expect(result).toEqual("Success save")
+    expect(mockEmployeeWorkType.create).toHaveBeenCalledWith(data)
+  })
+
+  it('should be delete', async () => {
+    const id = 1
+    mockEmployeeWorkType.delete.mockResolvedValue('Success delete')
+
+    const result = await service.deleteWorkType(id)
+
+    expect(result).toEqual('Success delete')
+  })
+
+  it('should be update', async () => {
+    const id = 1
+    const data: Partial<EmployeeWorkType> = {
+      title: "Mock Work Type",
+      price: 500,
+      workRequest: [new WorkRequest()],
+      employeeType: [new EmployeeType]
+    }
+    mockEmployeeWorkType.update.mockResolvedValue('Update success')
+    const result = await service.patchWorkType(id, data)
+
+    expect(result).toEqual('Update success')
+  })
 });
+
